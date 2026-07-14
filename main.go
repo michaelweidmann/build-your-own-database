0367755641cb456bd3526757eeb9d205b8af47f6
Michael Weidmann <michaelweidmann@web.de>
2026-07-14 14:06:10 +0200

    finish chapter 1: build simple REPL loop

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	for {
+		printPrompt()
+		input := readInput()
+
+		if input == ".exit" {
+			return
+		}
+
+		fmt.Printf("Unrecognized command '%s'.\n", input)
+	}
+}
+
+func printPrompt() {
+	fmt.Print("db > ")
+}
+
+func readInput() string {
+	var input string
+
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		log.Fatalln("Unexpected error occurred: ", err)
+	}
+
+	return input
 }
