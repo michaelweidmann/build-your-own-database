2bfd023b9afa1ee6a311bf08c4f8edb92d01a4b3
Michael Weidmann <michaelweidmann@web.de>
2026-07-14 16:39:22 +0200

    finish chapter 2: extend REPL loop by adding meta commands and parsing and execution of statements

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
+	"os"
+	"strings"
 )
 
+const ExitSuccessCode = 0
+
+type StatementType int
+
+const (
+	Insert StatementType = iota
+	Select
+)
+
+type Statement struct {
+	statementType StatementType
+}
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		printPrompt()
-		input := readInput()
+		input := readInput(scanner)
 
-		if input == ".exit" {
-			return
+		if input[0] == '.' {
+			err := executeMetaCommand(input)
+
+			if err != nil {
+				fmt.Printf(err.Error())
+				continue
+			}
 		}
 
-		fmt.Printf("Unrecognized command '%s'.\n", input)
+		statement, err := prepareStatement(input)
+
+		if err != nil {
+			fmt.Printf(err.Error())
+			continue
+		}
+
+		executeStatement(statement)
+		fmt.Println("Executed.")
 	}
 }
 
@@ -22,13 +52,36 @@ func printPrompt() {
 	fmt.Print("db > ")
 }
 
-func readInput() string {
-	var input string
+func readInput(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	input := scanner.Text()
+	return input
+}
 
-	_, err := fmt.Scanln(&input)
-	if err != nil {
-		log.Fatalln("Unexpected error occurred: ", err)
+func executeMetaCommand(input string) error {
+	if input == ".exit" {
+		os.Exit(ExitSuccessCode)
 	}
 
-	return input
+	return fmt.Errorf("Unrecognized command '%s'.\n", input)
+}
+
+func prepareStatement(input string) (Statement, error) {
+	if strings.HasPrefix(input, "insert") {
+		return Statement{Insert}, nil
+	}
+
+	if strings.HasPrefix(input, "select") {
+		return Statement{Select}, nil
+	}
+
+	return Statement{}, fmt.Errorf("Unrecognized keyword at start of '%s'.\n", input)
+}
+func executeStatement(statement Statement) {
+	switch statement.statementType {
+	case Insert:
+		fmt.Println("This is an insert!")
+	case Select:
+		fmt.Println("This is a select!")
+	}
 }
